9659324055d4df62de5dc63f385f24b3fc8da1c7
Steven Petryk <me@stevenpetryk.com>
2017-01-15 14:30:00 -0500

    Make database configuration easy (#12)

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :streamr, Streamr.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "streamr_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :streamr, Streamr.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "streamr_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
