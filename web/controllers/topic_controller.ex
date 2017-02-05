9daee6897938a55af4c0cd9ef2405a3d722f040c
Greg White <gkwhite95@gmail.com>
2017-02-05 15:13:06 -0500

    Add ordering and pagination to Topics

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,7 +2,11 @@ defmodule Streamr.TopicController do
   use Streamr.Web, :controller
   alias Streamr.{Topic, Repo}
 
-  def index(conn, _params) do
-    render(conn, "index.json-api", data: Repo.all(Topic))
+  def index(conn, params) do
+    topics = Topic
+    |> Topic.ordered
+    |> Repo.paginate(params)
+
+    render(conn, "index.json-api", data: topics)
   end
 end
