a2f4073bcaa409157fc3c0231a17d5c97755238b
Greg White <gkwhite95@gmail.com>
2017-02-05 15:10:49 -0500

    Add pagination for Streams

--- web/models/stream.ex
+++ web/models/stream.ex
@@ -1,5 +1,7 @@
 defmodule Streamr.Stream do
   use Streamr.Web, :model
+  import Ecto.Query
+
 
   schema "streams" do
     belongs_to :user, Streamr.User
@@ -8,5 +10,18 @@ defmodule Streamr.Stream do
     field :image, :string
 
     timestamps
+  end
+
+  def with_users(query) do
+    from(
+      stream in query,
+      preload: [:user],
+      select: stream
+    )
+  end
+
+  def ordered(query) do
+    from stream in query,
+    order_by: [asc: stream.id]
   end
 end
