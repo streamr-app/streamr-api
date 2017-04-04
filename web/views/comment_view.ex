4a227899a44d08fd86688e03c53567fe87872808
Greg White <gkwhite95@gmail.com>
2017-04-04 19:48:11 -0400

    Serialize comment inserted_at (#45)

--- web/views/comment_view.ex
+++ web/views/comment_view.ex
@@ -2,6 +2,6 @@ defmodule Streamr.CommentView do
   use Streamr.Web, :view
   use JaSerializer.PhoenixView
 
-  attributes [:body]
+  attributes [:body, :inserted_at]
   has_one :user, serializer: Streamr.UserView, include: true
 end
