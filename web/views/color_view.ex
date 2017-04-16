b5eb6033c272f6335b263d05633f43faa2093ab8
Greg White <gkwhite95@gmail.com>
2017-04-16 11:23:51 -0400

    Serialize color order (#77)

--- web/views/color_view.ex
+++ web/views/color_view.ex
@@ -2,7 +2,7 @@ defmodule Streamr.ColorView do
   use Streamr.Web, :view
   use JaSerializer.PhoenixView
 
-  attributes [:hex, :normal, :deuteranopia, :tritanopia, :protanopia]
+  attributes [:hex, :normal, :deuteranopia, :tritanopia, :protanopia, :order]
 
   def hex(color, %{assigns: %{current_user: nil}}), do: color.normal
   def hex(color, %{assigns: %{current_user: user}}), do: Map.get(color, user.color_preference)
